d58e32972bf2292c99c79a2c537a52f44e6c5271
Isack Ramirez <isack.ramirez1@gmail.com>
2021-09-07 17:34:30 -0500

    editing connection for heroku

--- Develop/server.js
+++ Develop/server.js
@@ -15,11 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/Workout", {
-  useNewUrlParser: true,
-  useCreateIndex : true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/Workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
