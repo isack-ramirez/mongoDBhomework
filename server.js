ccc5988fe39e641c55657fbf79e3c55edd34065d
Isack Ramirez <isack.ramirez1@gmail.com>
2021-09-07 22:03:49 -0500

    debugging heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-// const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -29,6 +29,6 @@ mongoose.connect(
 app.use(require("./routes/api.js"));
 app.use(require("./routes/homeRoutes.js"));
 
-app.listen( () => {
-  console.log(`App running !`);
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
 });
